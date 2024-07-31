0df8b21dc5d7170b1e16b7c6fbb901a59d6d7f1c
laldinenga <laldinenga11@email.com>
2024-07-31 11:59:38 +1000

    add drodownd button and screening questions

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import { Route, BrowserRouter as Router, Routes, Navigate } from "react-router-dom";
-import './App.css'
-import JobPosting from './JobPostingForm/JobPosting.jsx'
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import './App.css';
+import JobPosting from './JobPostingForm/JobPosting.jsx';
+import ScreeningQuestions from './JobPostingForm/ScreeningQuestions.jsx'
 
 function App() {
 
   return (
-    <Router>
-      <JobPosting />
-    </Router>
+    <main>
+      <Router>
+        <Routes>
+          <Route exact path="/JobPosting" element={<JobPosting />}></Route>
+          <Route exact path="/Screening" element={<ScreeningQuestions />}></Route>
+        </Routes>
+      </Router>
+    </main>
     
   )
 }
--- src/JobPostingForm/JobPosting.jsx
+++ src/JobPostingForm/JobPosting.jsx
@@ -111,21 +111,21 @@ export default function JobPosting() {
                                 />
                             </div>
                         </div>
-                        <div>
-                            <div className="flex items-center justify-between">
-                                <label htmlFor="workplace type" className="block text-sm font-medium leading-6 text-gray-900">
-                                    Workplace Type
-                                </label>
-                            </div>
+                        <div className="sm:col-span-3">
+                            <label htmlFor="workplace type" className="flex text-sm font-medium leading-6 text-gray-900">
+                                Workplace Type
+                            </label>
                             <div className="mt-2">
-                                <input
-                                    id="workplace type"
-                                    name="workplace type"
-                                    type="workplace type"
-                                    required
-                                    autoComplete="workplace type"
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                />
+                                <select
+                                id="workplace type"
+                                name="workplace type"
+                                autoComplete="workplace type"
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                                >
+                                <option>Remote</option>
+                                <option>On-site</option>
+                                <option>Hybrid</option>
+                                </select>
                             </div>
                         </div>
                         <div>
@@ -145,45 +145,26 @@ export default function JobPosting() {
                                 />
                             </div>
                         </div>
-                        {/* <Listbox value={selected} onChange={setSelected}>
-                            <Label className="block text-sm font-medium leading-6 text-gray-900">Assigned to</Label>
-                            <div className="relative mt-2">
-                                <ListboxButton className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
-                                    <span className="flex items-center">
-                                        <img alt="" src={selected.avatar} className="h-5 w-5 flex-shrink-0 rounded-full" />
-                                        <span className="ml-3 block truncate">{selected.name}</span>
-                                    </span>
-                                    <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
-                                        <ChevronUpDownIcon aria-hidden="true" className="h-5 w-5 text-gray-400" />
-                                    </span>
-                                </ListboxButton>
-
-                                <ListboxOptions
-                                    transition
-                                    className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none data-[closed]:data-[leave]:opacity-0 data-[leave]:transition data-[leave]:duration-100 data-[leave]:ease-in sm:text-sm"
+                        <div className="sm:col-span-3">
+                            <label htmlFor="jobtype" className="flex text-sm font-medium leading-6 text-gray-900">
+                                Job Type
+                            </label>
+                            <div className="mt-2 ">
+                                <select
+                                id="jobtype"
+                                name="jobtype"
+                                autoComplete="jobtypes"
+                                className="flex w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
                                 >
-                                    {people.map((person) => (
-                                        <ListboxOption
-                                            key={person.id}
-                                            value={person}
-                                            className="group relative cursor-default select-none py-2 pl-3 pr-9 text-gray-900 data-[focus]:bg-indigo-600 data-[focus]:text-white"
-                                        >
-                                            <div className="flex items-center">
-                                                <img alt="" src={person.avatar} className="h-5 w-5 flex-shrink-0 rounded-full" />
-                                                <span className="ml-3 block truncate font-normal group-data-[selected]:font-semibold">
-                                                    {person.name}
-                                                </span>
-                                            </div>
-
-                                            <span className="absolute inset-y-0 right-0 flex items-center pr-4 text-indigo-600 group-data-[focus]:text-white [.group:not([data-selected])_&]:hidden">
-                                                <CheckIcon aria-hidden="true" className="h-5 w-5" />
-                                            </span>
-                                        </ListboxOption>
-                                    ))}
-                                </ListboxOptions>
+                                <option>Internship</option>
+                                <option>Part-Time</option>
+                                <option>Full-Time</option>
+                                <option>Casual</option>
+                                <option>Others</option>
+                               
+                                </select>
                             </div>
-                        </Listbox> */}
-
+                        </div>
                         <div>
                             <button
                                 type="submit"
